0fb3aa198cd8504250992904fce652275d0f7d7e
saysayo <sayobamigbade@gmail.com>
2022-06-21 11:36:51 +0100

    implemented a few commands

---
+++ src-bindings/vscode/vscode.mli
@@ -0,0 +1,42 @@
+module Disposable : sig
+  type t
+
+  val t_to_js : t -> Ojs.t
+
+  val t_of_js : Ojs.t -> t
+end
+
+module ExtensionContext : sig
+  type t
+
+  val t_to_js : t -> Ojs.t
+
+  val t_of_js : Ojs.t -> t
+
+  val subscriptions : t -> Disposable.t list [@@js.get]
+
+  [@@@js.stop]
+  val subscribe : t -> disposable:Disposable.t -> unit
+  [@@@js.start]
+
+  [@@@js.implem
+  let subscribe this ~disposable =
+    let subscriptions = Ojs.get_prop_ascii ([%js.of: t] this) "subscriptions" in
+    let (_ : Ojs.t) =
+      Ojs.call subscriptions "push" [| [%js.of: Disposable.t] disposable |]
+    in
+    ()
+  ]
+end
+
+module Commands : sig
+  val registerCommand :
+    command:string
+    -> callback:(args:(Ojs.t list[@js.variadic]) -> unit)
+    -> Disposable.t
+    [@@js.global "vscode.commands.registerCommand"]
+end
+
+module Window : sig
+  val showInformationMessage : message:string -> unit [@@js.global "vscode.window.showInformationMessage"]
+end
---
+++ src/extension.ml
@@ -0,0 +1,12 @@
+let activate (context : Vscode.ExtensionContext.t) =
+  let disposable =
+    Vscode.Commands.registerCommand ~command:"extension.helloWorld" ~callback:(fun ~args:_ ->
+        Vscode.Window.showInformationMessage ~message:"Hello World!")
+  in
+  Vscode.ExtensionContext.subscribe ~disposable context
+
+(* see {{:https://code.visualstudio.com/api/references/vscode-api#Extension}
+   activate() *)
+let () =
+  let open Js_of_ocaml.Js in
+  export "activate" (wrap_callback activate)
